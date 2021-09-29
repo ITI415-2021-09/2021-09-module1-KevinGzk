70e807a09056c9f18cafe5ba15f4d85df72bd5f9
Zekai Gong <59636500+KevinGzk@users.noreply.github.com>
2021-09-29 18:12:17 -0400

    Cinemachine try

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    private Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
